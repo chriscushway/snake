9812864f17fe5ca892ce1625375927bf9d30c3ab
chris <christopherc@dpogroup.com>
2021-05-12 17:41:12 +0200

    add boilerplate for movements

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ const grid = new Grid(canvas, ctx, gridWidth, gridHeight);
 function init() {
 
 }
+
+
+document.onkeydown = handleKeyPress;
+
+function handleKeyPress(e) {
+    switch (e.keyCode) {
+        case 37:
+            console.log('left');
+            break;
+        case 38:
+           console.log('up');
+            break;
+        case 39:
+            console.log('right');
+            break;
+        case 40:
+            console.log('down');
+            break;
+    }
+}
